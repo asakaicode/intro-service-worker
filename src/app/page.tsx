69eddd52fc9ddeee8e6befbe38316d0dff690882
asakaicode <tubakaito5100@gmail.com>
2025-02-18 15:36:03 +0900

    fix: add interface to root page to display notifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+'use client'
+
 export default function Home() {
-  return <h1>サービスワーカーでのPush通知の実装</h1>
+  const onClickToPushNotification = async () => {
+    await fetch('/api/push/execute', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        body: 'Push通知の本文',
+      }),
+    })
+  }
+
+  return (
+    <>
+      <h1>サービスワーカーでのPush通知の実装</h1>
+      <button onClick={onClickToPushNotification}>通知を送ってもらう</button>
+    </>
+  )
 }
